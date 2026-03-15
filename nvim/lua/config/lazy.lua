074ea1503c9dbf1d8092d06cac8304914010aed0
Movsar Lamro <movsar@Movsars-MacBook-Pro.local>
2026-03-15 21:06:33 +0300

    added go lsp

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,6 +18,8 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- Go support (LSP, treesitter, formatting, etc.)
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- import/override with your plugins
     { import = "plugins" },
   },
