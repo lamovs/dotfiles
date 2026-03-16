c7ec4aa1456dbfc76656b369ea1b9788e1a75751
Movsar Lamro <movsar@Movsars-MacBook-Pro.local>
2026-03-17 00:40:57 +0300

    add hotkeys

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,3 +7,15 @@ vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "window righ
 vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { desc = "window down" })
 vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { desc = "window up" })
 vim.keymap.set("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<CR>", { desc = "window previous" })
+
+-- Add new line below, cursor stays
+vim.keymap.set("n", "<CR>", "<cmd>call append(line('.'), '')<CR>", { desc = "add line below" })
+
+-- Add new line above, cursor stays
+vim.keymap.set("n", "<S-CR>", "<cmd>call append(line('.')-1, '')<CR>", { desc = "add line above" })
+
+-- Add new line below, cursor moves there
+vim.keymap.set("n", "zj", "<cmd>call append(line('.'), '')<CR>j", { desc = "add line below and move cursor" })
+
+-- Add new line above, cursor moves there
+vim.keymap.set("n", "zk", "<cmd>call append(line('.')-1, '')<CR>k", { desc = "add line above and move cursor" })
