1233a20d9971e1e61fa915375ee40d046811a763
Movsar Lamro <movsar@Movsars-MacBook-Pro.local>
2026-03-18 02:24:18 +0300

    clear

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -12,6 +12,33 @@ return {
     priority = 1000,
     opts = {
       flavour = "frappe",
+      -- TODO: Remove this block if you stop using catppuccin-frappe.
+      -- It only makes Snacks Explorer match the editor background.
+      custom_highlights = function(colors)
+        local panel = colors.base
+        local border = colors.surface1
+
+        return {
+          SnacksPicker = { fg = colors.text, bg = panel },
+          SnacksPickerInput = { fg = colors.text, bg = panel },
+          SnacksPickerList = { fg = colors.text, bg = panel },
+          SnacksPickerPreview = { fg = colors.text, bg = panel },
+          SnacksPickerBox = { bg = panel },
+          SnacksPickerBorder = { fg = border, bg = panel },
+          SnacksPickerInputBorder = { fg = border, bg = panel },
+          SnacksPickerListBorder = { fg = border, bg = panel },
+          SnacksPickerPreviewBorder = { fg = border, bg = panel },
+          SnacksPickerBoxBorder = { fg = border, bg = panel },
+          SnacksPickerTitle = { fg = colors.blue, bg = panel },
+          SnacksPickerInputTitle = { fg = colors.blue, bg = panel },
+          SnacksPickerListTitle = { fg = colors.blue, bg = panel },
+          SnacksPickerPreviewTitle = { fg = colors.blue, bg = panel },
+          SnacksPickerFooter = { fg = colors.overlay1, bg = panel },
+          SnacksPickerInputFooter = { fg = colors.overlay1, bg = panel },
+          SnacksPickerListFooter = { fg = colors.overlay1, bg = panel },
+          SnacksPickerPreviewFooter = { fg = colors.overlay1, bg = panel },
+        }
+      end,
       integrations = {
         lualine = {
           all = function(colors)
