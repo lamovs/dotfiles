1405e99b83eb8cb0d5bd8372766bb84ad4e9acbe
Movsar Lamro <movsar@Movsars-MacBook-Pro.local>
2026-03-19 01:36:32 +0300

    fish: clear login banner on startup

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,11 @@ set -gx EDITOR nvim
 set -gx VISUAL nvim
 set -g fish_greeting
 
+# Hide macOS's "Last login" banner in interactive login shells.
+if status is-interactive; and status is-login
+    clear
+end
+
 # Mise (runtime manager)
 mise activate fish | source
 
